9b40131a0030946826c23177cc822b8c274ff96b
Gick <71695624+GICK00@users.noreply.github.com>
2021-12-12 15:45:09 +0300

    Add files via upload

---
+++ Form_Z08/Form_Z08/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace Form_Z08
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxStr = new System.Windows.Forms.TextBox();
+            this.buttonResult = new System.Windows.Forms.Button();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(298, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Находение максимального числа.";
+            // 
+            // textBoxStr
+            // 
+            this.textBoxStr.Location = new System.Drawing.Point(16, 39);
+            this.textBoxStr.Multiline = true;
+            this.textBoxStr.Name = "textBoxStr";
+            this.textBoxStr.Size = new System.Drawing.Size(294, 97);
+            this.textBoxStr.TabIndex = 1;
+            // 
+            // buttonResult
+            // 
+            this.buttonResult.Location = new System.Drawing.Point(124, 142);
+            this.buttonResult.Name = "buttonResult";
+            this.buttonResult.Size = new System.Drawing.Size(75, 23);
+            this.buttonResult.TabIndex = 2;
+            this.buttonResult.Text = "Найти";
+            this.buttonResult.UseVisualStyleBackColor = true;
+            this.buttonResult.Click += new System.EventHandler(this.buttonResult_Click);
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.BackColor = System.Drawing.SystemColors.Control;
+            this.textBoxResult.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxResult.Location = new System.Drawing.Point(16, 171);
+            this.textBoxResult.Multiline = true;
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.Size = new System.Drawing.Size(294, 28);
+            this.textBoxResult.TabIndex = 3;
+            this.textBoxResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Утин Д. С.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 237);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.buttonResult);
+            this.Controls.Add(this.textBoxStr);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form Z08";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxStr;
+        private System.Windows.Forms.Button buttonResult;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form_Z08/Form_Z08/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Form_Z08
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonResult_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StringBuilder str = new StringBuilder(Convert.ToString(textBoxStr.Text));
+                if (str.Length == 0)
+                    throw new Exception("Строка должна содержать хотябы 1 символ!");
+
+                max(str);
+            }
+            catch (Exception ex)
+            {
+                textBoxResult.Text = $"{ex.Message}";
+            }
+        }
+        public void max(StringBuilder str)
+        {
+            int max = 0;
+            Regex reg = new Regex(@"(?<!\d+)-?\d+");
+            MatchCollection maxNum = reg.Matches(str.ToString());
+            foreach (Match match in maxNum)
+            {
+                if (int.Parse(match.Value) > max)
+                {
+                    max = int.Parse(match.Value);
+                }
+            }
+            textBoxResult.Text = "Максимально число в строке: " + max;
+        }
+    }
+}
